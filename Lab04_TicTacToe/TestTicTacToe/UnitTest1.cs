c10bd222beff58f7b4a6793707f39153ed796f4c
ellisken <kse104@gmail.com>
2018-10-12 10:08:12 -0700

    add test suite comments

---
+++ Lab04_TicTacToe/TestTicTacToe/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace TestTicTacToe
+{
+    public class UnitTest1
+    {
+        /* REQUIRED TESTS:
+         * Given a game board, test for winners (3 different tests) [Theory]
+         * Test for switch in players between turns
+         * Confirm player position correlates to correct index of the board array
+         * Create one other test of your own
+         */
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
